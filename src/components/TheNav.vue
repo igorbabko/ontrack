1ceaa21da50bc5a7fe72ab7bf278dd6cae12ddf0
Igor Babko <i.i.babko@gmail.com>
2023-03-24 09:38:29 +0200

    wip

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -4,10 +4,36 @@ const emit = defineEmits(['go']);
 
 <template>
   <nav class="sticky bottom-0 bg-white">
-    <ul class="flex justify-around items-center list-style-none h-16 border-t w-screen">
-      <li><a href="#timeline" @click="emit('go', 'timeline')">Timeline</a></li>
-      <li><a href="#activities" @click="emit('go', 'activities')">Activities</a></li>
-      <li><a href="#stats" @click="emit('go', 'stats')">Stats</a></li>
+    <ul class="flex justify-around items-center list-style-none border-t w-screen">
+      <li class="flex-1">
+        <a class="flex flex-col items-center p-2" href="#timeline" @click="emit('go', 'timeline')">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+          Timeline
+        </a>
+      </li>
+      <li class="flex-1">
+        <a class="flex flex-col items-center p-2" href="#activities" @click="emit('go', 'activities')">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+          </svg>
+          Activities
+        </a>
+      </li>
+      <li class="flex-1">
+        <a class="flex flex-col items-center p-2" href="#stats" @click="emit('go', 'stats')">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+            class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z" />
+          </svg>
+          Goals
+        </a>
+      </li>
       <!-- <li><a href="#goals" @click="emit('go', 'goals')">Goals</a></li> -->
     </ul>
   </nav>
