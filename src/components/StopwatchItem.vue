dc421f0aa1f8acbb228f3d3a586b25f3fd6c1c35
Igor Babko <i.i.babko@gmail.com>
2023-03-24 11:44:19 +0200

    wip

--- src/components/StopwatchItem.vue
+++ src/components/StopwatchItem.vue
@@ -1,33 +1,61 @@
 <script setup>
-import { useStopwatch } from '../composables/stopwatch';
+import { ref, computed } from 'vue';
+import StopwatchButtonReset from './StopwatchButtonReset.vue';
+import StopwatchButtonStart from './StopwatchButtonStart.vue';
+import StopwatchButtonStop from './StopwatchButtonStop.vue';
+import StopwatchTime from './StopwatchTime.vue';
 
 const props = defineProps(['time']);
 
-const stopwatch = useStopwatch(props.time);
+const isRunning = ref(false);
+
+let stopwatch = null;
+
+const time = ref(props.time);
+
+function start() {
+  isRunning.value = true;
+
+  stopwatch = setInterval(() => {
+    time.value++;
+  }, 1000);
+}
+
+function reset() {
+  time.value = 0;
+}
+
+function pause() {
+  isRunning.value = false;
+
+  clearInterval(stopwatch);
+}
+
+// function stop() {
+//   timerState.value = 'stopped';
+//   time.value = 0;
+
+//   clearInterval(stopwatch);
+// }
+
+const date = new Date(null);
+
+const formattedTime = computed(() => {
+  date.setTime(time.value * 1000);
+
+  const utc = date.toUTCString();
+
+  return utc.substring(utc.indexOf(':') - 2, utc.indexOf(':') + 6);
+});
+
+const shouldShowTime = computed(() => time.value || isRunning.value);
 </script>
 
 <template>
-  <div v-if="stopwatch.time.value || stopwatch.state.value !== 'paused'" class="flex">
-    <button class="mr-2" @click="stopwatch.reset">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-        class="w-5 h-5">
-        <path stroke-linecap="round" stroke-linejoin="round"
-          d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
-      </svg>
-    </button>
-    <div class="font-mono mr-6">{{ stopwatch.formattedTime.value }}</div>
-  </div>
-  <button v-if="stopwatch.state.value === 'running'" @click="stopwatch.pause">
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-      class="w-6 h-6">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
-    </svg>
-  </button>
-  <button v-else @click="stopwatch.start">
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-      class="w-6 h-6">
-      <path stroke-linecap="round" stroke-linejoin="round"
-        d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
-    </svg>
-  </button>
+  <template v-if="shouldShowTime">
+    <StopwatchButtonReset @click="reset" />
+    <StopwatchTime>{{ formattedTime }}</StopwatchTime>
+  </template>
+  <StopwatchButtonStop v-if="isRunning" @click="pause" />
+  <StopwatchButtonStart v-else @click="start" />
 </template>
---
+++ src/components/StopwatchTime.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="font-mono mr-6"><slot /></div>
+</template>
