bddc365f32712d8c17533f50797ed1086bfef931
Igor Babko <i.i.babko@gmail.com>
2023-03-28 14:27:14 +0300

    wip

--- src/components/TimelineHour.vue
+++ src/components/TimelineHour.vue
@@ -1,13 +1,7 @@
-<script setup>
-import { getCurrentHour } from '../functions';
-
-defineProps(['hour']);
-</script>
-
 <template>
   <a
     href="#"
-    :class="`absolute -top-4 left-1/2 -translate-x-1/2 font-mono text-lg text-gray-500 bg-gray-100 rounded px-2 ${hour === getCurrentHour() ? 'bg-purple-900 text-white font-black' : ''}`">
-    {{ hour.toString().padStart(2, 0) }}:00
+    class="absolute -top-4 left-1/2 -translate-x-1/2 font-mono text-lg text-gray-500 bg-gray-100 rounded px-2">
+    <slot></slot>
   </a>
 </template>
--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -1,17 +1,24 @@
 <script setup>
+import { computed } from 'vue';
 import { getCurrentHour } from '../functions';
 import BaseSelect from './BaseSelect.vue'
 import TimelineHour from './TimelineHour.vue';
 import TimelineStopwatch from './TimelineStopwatch.vue';
 
-defineProps(['timelineItem', 'activitySelectOptions']);
+const props = defineProps(['timelineItem', 'activitySelectOptions']);
 
 const emit = defineEmits(['selectActivity', 'updateActivitySeconds', 'scrollTo']);
+
+const hour = computed(() => `${props.timelineItem.hour.toString().padStart(2, 0)}:00`);
 </script>
 
 <template>
   <li class="py-10 px-4 flex flex-col gap-2 relative border-t border-gray-200">
-    <TimelineHour :hour="timelineItem.hour" @click.prevent="emit('scrollTo')" />
+    <TimelineHour
+      :class="{ 'bg-purple-900 text-white font-black': timelineItem.hour === getCurrentHour() }"
+      @click.prevent="emit('scrollTo')">
+      {{ hour }}
+    </TimelineHour>
     <BaseSelect
       :selected="timelineItem.activityId"
       :options="activitySelectOptions"
