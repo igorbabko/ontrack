5f51ce2d457d379235420cb730a5da0f1be9eacd
Igor Babko <i.i.babko@gmail.com>
2023-03-28 11:05:25 +0300

    wip

--- src/components/TimelineHour.vue
+++ src/components/TimelineHour.vue
@@ -7,7 +7,7 @@ defineProps(['hour']);
 <template>
   <a
     href="#"
-    :class="`absolute -top-4 left-1/2 -translate-x-1/2 font-mono text-lg text-gray-500 bg-gray-100 rounded px-1 ${hour === getCurrentHour() ? 'bg-purple-900 text-white font-black' : ''}`">
+    :class="`absolute -top-4 left-1/2 -translate-x-1/2 font-mono text-lg text-gray-500 bg-gray-100 rounded px-2 ${hour === getCurrentHour() ? 'bg-purple-900 text-white font-black' : ''}`">
     {{ hour.toString().padStart(2, 0) }}:00
   </a>
 </template>
--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -1,19 +1,25 @@
 <script setup>
+import { computed } from 'vue';
+import { getCurrentHour, generateActivitySelectOptions } from '../functions';
+import BaseSelect from './BaseSelect.vue'
 import TimelineHour from './TimelineHour.vue';
-import TimelineActivity from './TimelineActivity.vue';
 import TimelineStopwatch from './TimelineStopwatch.vue';
 
 defineProps(['timelineItem', 'activities']);
 
 const emit = defineEmits(['selectActivity', 'updateActivitySeconds', 'scrollTo']);
+
+const activitySelectOptions = computed(() => generateActivitySelectOptions(props.activities));
 </script>
 
 <template>
   <li class="py-10 px-4 flex flex-col gap-1 relative border-t border-gray-200">
     <TimelineHour :hour="timelineItem.hour" @click.prevent="emit('scrollTo')" />
-    <TimelineActivity
-      :timeline-item="timelineItem"
-      :activities="activities"
+    <BaseSelect
+      :selected="timelineItem.activityId"
+      :options="activitySelectOptions"
+      :disabled="timelineItem.hour < getCurrentHour()"
+      placeholder="Rest"
       @select="emit('selectActivity', $event)" />
     <TimelineStopwatch
       :timeline-item="timelineItem"
