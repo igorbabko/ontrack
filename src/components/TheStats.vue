f2551a04f9b4a304a74227c02d0c81305e1288a2
Igor Babko <i.i.babko@gmail.com>
2023-03-30 12:36:55 +0300

    wip

--- src/components/TheStats.vue
+++ src/components/TheStats.vue
@@ -3,19 +3,38 @@ import { computed } from 'vue';
 import { formatTime, getTotalActivitySeconds } from '../functions';
 
 const props = defineProps(['activities', 'timelineItems']);
+
+function color(activity) {
+  const p = percentage(activity);
+
+  if (p < 33) return 'bg-red-600';
+
+  if (p < 66) return 'bg-orange-600';
+
+  if (p < 100) return 'bg-blue-600';
+
+  return 'bg-green-600';
+}
+
+function percentage(activity) {
+  return Math.round(getTotalActivitySeconds(activity, props.timelineItems) * 100 / activity.secondsToComplete);
+}
 </script>
 
 <template>
-  <div class="p-4 space-y-5">
-    <div v-for="activity in activities.filter(({ secondsToComplete }) => secondsToComplete)" class="flex flex-col gap-1">
+  <div class="divide-y">
+    <div v-for="activity in activities.filter(({ secondsToComplete }) => secondsToComplete)"
+      class="flex flex-col gap-1 p-4">
       <label for="file" class="text-xl">{{ activity.name }}</label>
-      <progress id="file" :max="activity.secondsToComplete" :value="getTotalActivitySeconds(activity, timelineItems)">
+      <progress id="file" :class="`rounded h-4 ${color(activity)} [&::-moz-progress-bar]:bg-purple-500`" :max="activity.secondsToComplete"
+        :value="getTotalActivitySeconds(activity, timelineItems)">
         70%
       </progress>
       <div class="flex justify-between text-sm font-mono">
-        <span>{{ Math.round(getTotalActivitySeconds(activity, timelineItems) * 100 / activity.secondsToComplete) }}%</span>
+        <span>{{ percentage(activity) }}%</span>
         <span>
-          {{ formatTime(getTotalActivitySeconds(activity, timelineItems)) }} / {{ formatTime(activity.secondsToComplete) }}
+          {{ formatTime(getTotalActivitySeconds(activity, timelineItems)) }} / {{ formatTime(activity.secondsToComplete)
+          }}
         </span>
       </div>
     </div>
