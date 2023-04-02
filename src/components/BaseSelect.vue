7acd09f99e74f816efdb21341ed67bcd2391f85d
Igor Babko <i.i.babko@gmail.com>
2023-04-02 17:38:49 +0300

    disabled select

--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -3,12 +3,7 @@ import { computed } from 'vue';
 import { XMarkIcon } from '@heroicons/vue/24/outline';
 import BaseButton from './BaseButton.vue'
 
-const props = defineProps({
-  selected: String | Number,
-  disabled: Boolean,
-  options: Array,
-  placeholder: String
-});
+const props = defineProps(['selected', 'options', 'placeholder']);
 
 const emit = defineEmits(['select']);
 
@@ -19,17 +14,13 @@ const isNotSelected = computed(() => [null, undefined].includes(props.selected))
   <div class="flex gap-2">
     <BaseButton
       type="neutral"
-      :disabled="disabled"
       @click="emit('select', null)">
       <XMarkIcon class="h-8" />
     </BaseButton>
     <select
-      class="w-full py-1 px-2 text-2xl rounded bg-gray-100 disabled:opacity-50 disabled:cursor-not-allowed truncate"
-      :disabled="disabled"
+      class="w-full py-1 px-2 text-2xl rounded bg-gray-100 truncate"
       @change="emit('select', $event.target.value)">
-      <option :selected="isNotSelected" disabled value="">
-        {{ placeholder }}
-      </option>
+      <option :selected="isNotSelected" disabled value="">{{ placeholder }}</option>
       <option
         v-for="{ value, label } in options"
         :key="value"
