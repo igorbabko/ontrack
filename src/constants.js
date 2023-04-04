82a2bf0a33b571432d0a284496435c2ed565c4e0
Igor Babko <i.i.babko@gmail.com>
2023-04-04 14:23:10 +0300

    constants

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,9 @@ export const APP_NAME = 'ontrack';
 export const PAGE_TIMELINE = 'timeline';
 export const PAGE_ACTIVITIES = 'activities';
 export const PAGE_PROGRESS = 'progress';
+
+export const MILLISECONDS_IN_SECOND = 1000;
+export const SECONDS_IN_MINUTE = 60;
+export const SECONDS_IN_HOUR = 86400;
+
+export const HUNDRED_PERCENT = 100;
