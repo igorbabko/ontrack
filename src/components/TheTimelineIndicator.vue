7458710cac43980d86dd828b5be7cabc77a74782
Igor Babko <i.i.babko@gmail.com>
2023-04-08 13:24:39 +0300

    wip

--- src/components/TheTimelineIndicator.vue
+++ src/components/TheTimelineIndicator.vue
@@ -46,5 +46,5 @@ function seconds() {
 </script>
 
 <template>
-  <hr ref="el" class="absolute border-b-2 border-red-600 w-full z-10 opacity-50">
+  <hr ref="el" class="absolute border-b-2 border-red-600 w-full z-10 opacity-50 pointer-events-none">
 </template>
