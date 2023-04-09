12f210f5e4dbafce952517a41f9751ed6984be91
Igor Babko <i.i.babko@gmail.com>
2023-04-09 12:28:02 +0300

    02. Header template

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
+<script setup>
+import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+</script>
+
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3">
+    <a href="#">
+      <img src="./assets/logo.png" alt="Logo" class="h-9" />
+    </a>
+    <a href="#" class="text-sm">
+      <div v-if="false" class="flex items-center gap-1">
+        Day complete!
+        <CheckCircleIcon class="h-7 text-green-500" />
+      </div>
+      <div v-else class="flex items-center gap-1">
+        <div>Progress: <span class="font-mono">20%</span></div>
+        <div class="h-3 w-3 rounded-full bg-red-500"></div>
+      </div>
+    </a>
+  </header>
 </template>
