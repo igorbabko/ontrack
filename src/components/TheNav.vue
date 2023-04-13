2f3b3c33bc3e610b7697cde91736b5e596a5cf71
Igor Babko <i.i.babko@gmail.com>
2023-04-13 11:01:14 +0300

    09. Normalize page hash

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,15 +1,28 @@
 <script setup>
 import { ref } from 'vue'
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from '../constants'
 import NavItem from './NavItem.vue'
 
 const navItems = {
-  timeline: ClockIcon,
-  activities: ListBulletIcon,
-  progress: ChartBarIcon
+  [PAGE_TIMELINE]: ClockIcon,
+  [PAGE_ACTIVITIES]: ListBulletIcon,
+  [PAGE_PROGRESS]: ChartBarIcon
 }
 
-const currentPage = ref('timeline')
+const currentPage = ref(normalizePageHash())
+
+function normalizePageHash() {
+  const hash = window.location.hash.slice(1)
+
+  if (Object.keys(navItems).includes(hash)) {
+    return hash
+  }
+
+  window.location.hash = PAGE_TIMELINE
+
+  return PAGE_TIMELINE
+}
 </script>
 
 <template>
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const PAGE_TIMELINE = 'timeline'
+export const PAGE_ACTIVITIES = 'activities'
+export const PAGE_PROGRESS = 'progress'
