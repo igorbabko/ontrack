a91b4c1a1fc1c8577792a1f48103f686a4d8f52b
Igor Babko <i.i.babko@gmail.com>
2023-04-13 10:04:34 +0300

    08. Highlight current nav item

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from 'vue'
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 import NavItem from './NavItem.vue'
 
@@ -7,12 +8,20 @@ const navItems = {
   activities: ListBulletIcon,
   progress: ChartBarIcon
 }
+
+const currentPage = ref('timeline')
 </script>
 
 <template>
   <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
-      <NavItem v-for="(icon, page) in navItems" :key="page" :href="`#${page}`">
+      <NavItem
+        v-for="(icon, page) in navItems"
+        :key="page"
+        :href="`#${page}`"
+        :class="{ 'pointer-events-none bg-gray-200': page === currentPage }"
+        @click="currentPage = page"
+      >
         <component :is="icon" class="h-6 w-6" /> {{ page }}
       </NavItem>
     </ul>
