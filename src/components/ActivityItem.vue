027e9de4e5482fce55c1d31b24b2ad2a69d04049
Igor Babko <i.i.babko@gmail.com>
2023-04-25 13:45:59 +0300

    27. Activity item component

---
+++ src/components/ActivityItem.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref } from 'vue'
+import { TrashIcon } from '@heroicons/vue/24/outline'
+import BaseButton from './BaseButton.vue'
+import BaseSelect from './BaseSelect.vue'
+
+defineProps({
+  activity: {
+    required: true,
+    type: String
+  }
+})
+
+const periodSelectOptions = [
+  {
+    value: 15,
+    label: '0:15'
+  },
+  {
+    value: 30,
+    label: '0:30'
+  },
+  {
+    value: 45,
+    label: '0:45'
+  }
+]
+
+const secondsToComplete = ref(null)
+</script>
+
+<template>
+  <li class="flex flex-col gap-2 p-4">
+    <div class="flex items-center gap-2">
+      <BaseButton>
+        <TrashIcon class="h-8" />
+      </BaseButton>
+      <span class="truncate text-xl">{{ activity }}</span>
+    </div>
+    <div>
+      <BaseSelect
+        class="font-mono"
+        placeholder="h:mm"
+        :options="periodSelectOptions"
+        :selected="secondsToComplete"
+        @select="secondsToComplete = $event"
+      />
+    </div>
+  </li>
+</template>
--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -1,40 +1,13 @@
 <script setup>
-import { TrashIcon } from '@heroicons/vue/24/outline'
-import BaseButton from '../components/BaseButton.vue'
-import BaseSelect from '../components/BaseSelect.vue'
+import ActivityItem from '../components/ActivityItem.vue'
 
 const activities = ['Coding', 'Reading', 'Training']
-
-const periodSelectOptions = [
-  {
-    value: 15,
-    label: '0:15'
-  },
-  {
-    value: 30,
-    label: '0:30'
-  },
-  {
-    value: 45,
-    label: '0:45'
-  }
-]
 </script>
 
 <template>
   <div>
     <ul class="divide-y">
-      <li v-for="activity in activities" :key="activity" class="flex flex-col gap-2 p-4">
-        <div class="flex items-center gap-2">
-          <BaseButton>
-            <TrashIcon class="h-8" />
-          </BaseButton>
-          <span class="truncate text-xl">{{ activity }}</span>
-        </div>
-        <div>
-          <BaseSelect class="font-mono" placeholder="h:mm" :options="periodSelectOptions" />
-        </div>
-      </li>
+      <ActivityItem v-for="activity in activities" :key="activity" :activity="activity" />
     </ul>
   </div>
 </template>
