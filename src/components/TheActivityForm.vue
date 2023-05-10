87af5aaef48be0308fb6b410c029b3d9d5842760
Igor Babko <i.i.babko@gmail.com>
2023-05-10 12:51:11 +0300

    36. v-model directive

--- src/components/TheActivityForm.vue
+++ src/components/TheActivityForm.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from 'vue'
 import { PlusIcon } from '@heroicons/vue/24/outline'
 import { isActivityValid } from '../validators'
 import BaseButton from './BaseButton.vue'
@@ -7,10 +8,12 @@ const emit = defineEmits({
   submit: isActivityValid
 })
 
-let activity = ''
+const activity = ref('')
 
 function submit() {
-  emit('submit', activity)
+  emit('submit', activity.value)
+
+  activity.value = ''
 }
 </script>
 
@@ -18,12 +21,11 @@ function submit() {
   <form @submit.prevent="submit" class="sticky bottom-[57px] flex gap-2 border-t bg-white p-4">
     <input
       type="text"
-      :value="activity"
-      @input="activity = $event.target.value"
+      v-model="activity"
       class="w-full rounded border px-4 text-xl"
       placeholder="Activity name"
     />
-    <BaseButton>
+    <BaseButton :disabled="activity.trim() === ''">
       <PlusIcon class="h-8" />
     </BaseButton>
   </form>
