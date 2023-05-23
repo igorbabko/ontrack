8e7ac415b80ec5e669531f0c71c5b2ab22b8b158
Igor Babko <i.i.babko@gmail.com>
2023-05-23 12:44:08 +0300

    52. Make stopwatch count

--- src/components/TimelineStopwatch.vue
+++ src/components/TimelineStopwatch.vue
@@ -1,31 +1,58 @@
 <script setup>
+import { ref } from 'vue'
 import { ArrowPathIcon, PauseIcon, PlayIcon } from '@heroicons/vue/24/outline'
-import { BUTTON_TYPE_SUCCESS, BUTTON_TYPE_WARNING, BUTTON_TYPE_DANGER } from '../constants'
+import {
+  MILLISECONDS_IN_SECOND,
+  BUTTON_TYPE_SUCCESS,
+  BUTTON_TYPE_WARNING,
+  BUTTON_TYPE_DANGER
+} from '../constants'
 import { formatSeconds } from '../functions'
 import { isNumber } from '../validators'
 import BaseButton from './BaseButton.vue'
 
-defineProps({
+const props = defineProps({
   seconds: {
     default: 0,
     type: Number,
     validator: isNumber
   }
 })
+
+const seconds = ref(props.seconds)
+const isRunning = ref(false)
+
+function start() {
+  isRunning.value = setInterval(() => {
+    seconds.value++
+  }, MILLISECONDS_IN_SECOND)
+}
+
+function stop() {
+  clearInterval(isRunning.value)
+
+  isRunning.value = false
+}
+
+function reset() {
+  stop()
+
+  seconds.value = 0
+}
 </script>
 
 <template>
   <div class="flex w-full gap-2">
-    <BaseButton :type="BUTTON_TYPE_DANGER">
+    <BaseButton :type="BUTTON_TYPE_DANGER" @click="reset">
       <ArrowPathIcon class="h-8" />
     </BaseButton>
     <div class="flex flex-grow items-center rounded bg-gray-100 px-2 font-mono text-3xl">
       {{ formatSeconds(seconds) }}
     </div>
-    <BaseButton :type="BUTTON_TYPE_WARNING">
+    <BaseButton v-if="isRunning" :type="BUTTON_TYPE_WARNING" @click="stop">
       <PauseIcon class="h-8" />
     </BaseButton>
-    <BaseButton :type="BUTTON_TYPE_SUCCESS">
+    <BaseButton v-else :type="BUTTON_TYPE_SUCCESS" @click="start">
       <PlayIcon class="h-8" />
     </BaseButton>
   </div>
