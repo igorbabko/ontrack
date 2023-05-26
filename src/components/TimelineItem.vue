f54a779b2e9b6bdad1a18637c23ffe535ae0990e
Igor Babko <i.i.babko@gmail.com>
2023-05-26 12:03:07 +0300

    56. Scroll to clicked hour

--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -3,6 +3,7 @@ import { NULLABLE_ACTIVITY } from '../constants'
 import {
   isTimelineItemValid,
   isActivityValid,
+  isHourValid,
   validateSelectOptions,
   validateActivities
 } from '../validators'
@@ -29,7 +30,8 @@ const props = defineProps({
 })
 
 const emit = defineEmits({
-  selectActivity: isActivityValid
+  selectActivity: isActivityValid,
+  scrollToHour: isHourValid
 })
 
 function selectActivity(id) {
@@ -43,7 +45,10 @@ function findActivityById(id) {
 
 <template>
   <li class="relative flex flex-col gap-2 border-t border-gray-200 py-10 px-4">
-    <TimelineHour :hour="timelineItem.hour" />
+    <TimelineHour
+      :hour="timelineItem.hour"
+      @click.prevent="emit('scrollToHour', timelineItem.hour)"
+    />
     <BaseSelect
       placeholder="Rest"
       :selected="timelineItem.activityId"
--- src/pages/TheTimeline.vue
+++ src/pages/TheTimeline.vue
@@ -46,17 +46,15 @@ watchPostEffect(async () => {
   if (props.currentPage === PAGE_TIMELINE) {
     await nextTick()
 
-    scrollToCurrentTimelineItem()
+    scrollToHour(new Date().getHours())
   }
 })
 
-function scrollToCurrentTimelineItem() {
-  const currentHour = new Date().getHours()
-
-  if (currentHour === MIDNIGHT_HOUR) {
+function scrollToHour(hour) {
+  if (hour === MIDNIGHT_HOUR) {
     document.body.scrollIntoView()
   } else {
-    timelineItemRefs.value[currentHour - 1].$el.scrollIntoView()
+    timelineItemRefs.value[hour - 1].$el.scrollIntoView()
   }
 }
 </script>
@@ -71,6 +69,7 @@ function scrollToCurrentTimelineItem() {
         :activities="activities"
         :activity-select-options="activitySelectOptions"
         ref="timelineItemRefs"
+        @scroll-to-hour="scrollToHour"
         @select-activity="emit('setTimelineItemActivity', timelineItem, $event)"
       />
     </ul>
