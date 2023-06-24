3c43407909f384a98f32a617e0693debfb9d7204
Igor Babko <i.i.babko@gmail.com>
2023-06-24 22:50:25 +0300

    x - baseIcon

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,33 @@
+<script setup>
+import {
+  CheckCircleIcon as CheckCircle,
+  ListBulletIcon as ListBullet,
+  ArrowPathIcon as ArrowPath,
+  ChartBarIcon as ChartBar,
+  ClockIcon as Clock,
+  TrashIcon as Trash,
+  XMarkIcon as XMark,
+  PauseIcon as Pause,
+  PlayIcon as Play,
+  PlusIcon as Plus
+} from '@heroicons/vue/24/outline'
+
+const icons = {
+  CheckCircle,
+  ListBullet,
+  ArrowPath,
+  ChartBar,
+  Clock,
+  Trash,
+  XMark,
+  Pause,
+  Play,
+  Plus
+}
+
+defineProps(['name'])
+</script>
+
+<template>
+  <component :is="icons[name]" />
+</template>
