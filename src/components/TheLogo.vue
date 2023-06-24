939773657ffd0fd193c707e4e35d0336990559dc
Igor Babko <i.i.babko@gmail.com>
2023-06-24 20:12:27 +0300

    x - refactor scrolling to hours

--- src/components/TheLogo.vue
+++ src/components/TheLogo.vue
@@ -1,10 +1,15 @@
 <script setup>
 import { PAGE_TIMELINE } from '../constants'
-import { navigate } from '../router'
+import { currentPage, navigate } from '../router'
+import { scrollToHour } from '../timeline-items'
+
+function handleClick() {
+  currentPage.value === PAGE_TIMELINE ? scrollToHour() : navigate(PAGE_TIMELINE)
+}
 </script>
 
 <template>
-  <a :href="`#${PAGE_TIMELINE}`" @click="navigate(PAGE_TIMELINE)">
+  <a :href="`#${PAGE_TIMELINE}`" @click="handleClick">
     <img src="../assets/img/logo.png" alt="Logo" class="h-9" />
   </a>
 </template>
--- src/pages/TheTimeline.vue
+++ src/pages/TheTimeline.vue
@@ -1,14 +1,9 @@
 <script setup>
-import { ref, watchPostEffect, nextTick } from 'vue'
-import { PAGE_TIMELINE, MIDNIGHT_HOUR } from '../constants'
-import { currentHour } from '../functions'
+import { watchPostEffect, nextTick } from 'vue'
+import { PAGE_TIMELINE } from '../constants'
 import { currentPage } from '../router'
-import { timelineItems } from '../timeline-items'
+import { timelineItems, timelineItemRefs, scrollToHour } from '../timeline-items'
 import TimelineItem from '../components/TimelineItem.vue'
-
-defineExpose({ scrollToHour })
-
-const timelineItemRefs = ref([])
 
 watchPostEffect(async () => {
   if (currentPage.value === PAGE_TIMELINE) {
@@ -17,14 +12,6 @@ watchPostEffect(async () => {
     scrollToHour(null, false)
   }
 })
-
-function scrollToHour(hour = null, isSmooth = true) {
-  hour ??= currentHour()
-
-  const el = hour === MIDNIGHT_HOUR ? document.body : timelineItemRefs.value[hour - 1].$el
-
-  el.scrollIntoView({ behavior: isSmooth ? 'smooth' : 'instant' })
-}
 </script>
 
 <template>
