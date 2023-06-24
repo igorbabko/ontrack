c8848bbb5fee7b90cb8876851c24da7e9f103c18
Igor Babko <i.i.babko@gmail.com>
2023-06-24 22:58:17 +0300

    .

--- src/components/TheHeaderProgress.vue
+++ src/components/TheHeaderProgress.vue
@@ -1,14 +1,14 @@
 <script setup>
-import { CheckCircleIcon } from '@heroicons/vue/24/solid'
 import { PAGE_PROGRESS } from '../constants'
 import { navigate } from '../router'
+import BaseIcon from './BaseIcon.vue'
 </script>
 
 <template>
   <a :href="`#${PAGE_PROGRESS}`" @click="navigate(PAGE_PROGRESS)" class="text-sm">
     <div v-if="false" class="flex items-center gap-1">
       Day complete!
-      <CheckCircleIcon class="h-7 text-green-500" />
+      <BaseIcon name="CheckCircle" class="h-7 text-green-500" />
     </div>
     <div v-else class="flex items-center gap-1">
       <div>Progress: <span class="font-mono">20%</span></div>
