607c134358fc977c9e400777b3e5da6954834ab4
Igor Babko <i.i.babko@gmail.com>
2023-06-24 13:43:04 +0300

    wip

--- src/components/ActivityItem.vue
+++ src/components/ActivityItem.vue
@@ -3,6 +3,7 @@ import { TrashIcon } from '@heroicons/vue/24/outline'
 import { BUTTON_TYPE_DANGER, PERIOD_SELECT_OPTIONS } from '../constants'
 import { isActivityValid } from '../validators'
 import { setActivitySecondsToComplete, deleteActivity } from '../activities'
+import { resetTimelineItemActivities } from '../timeline-items'
 import BaseButton from './BaseButton.vue'
 import BaseSelect from './BaseSelect.vue'
 import ActivitySecondsToComplete from './ActivitySecondsToComplete.vue'
@@ -14,12 +15,17 @@ defineProps({
     validator: isActivityValid
   }
 })
+
+function deleteAndResetActivity(activity) {
+  resetTimelineItemActivities(activity)
+  deleteActivity(activity)
+}
 </script>
 
 <template>
   <li class="flex flex-col gap-2 p-4">
     <div class="flex items-center gap-2">
-      <BaseButton :type="BUTTON_TYPE_DANGER" @click="deleteActivity(activity)">
+      <BaseButton :type="BUTTON_TYPE_DANGER" @click="deleteAndResetActivity(activity)">
         <TrashIcon class="h-8" />
       </BaseButton>
       <span class="truncate text-xl">{{ activity.name }}</span>
