2caa491d606b60461e8339c9faf2b827f71dc3dd
Igor Babko <i.i.babko@gmail.com>
2023-07-04 12:15:16 +0300

    71. Integrate router module

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import {
   generateActivitySelectOptions,
   generatePeriodSelectOptions
 } from './functions'
-import { currentPage, navigate, timelineRef } from './router'
+import { currentPage, timelineRef } from './router'
 import TheHeader from './components/TheHeader.vue'
 import TheNav from './components/TheNav.vue'
 import TheTimeline from './pages/TheTimeline.vue'
@@ -58,18 +58,17 @@ provide('timelineItems', timelineItems.value)
 </script>
 
 <template>
-  <TheHeader @navigate="navigate" />
+  <TheHeader />
 
   <main class="flex flex-grow flex-col">
     <TheTimeline
       v-show="currentPage === PAGE_TIMELINE"
       :timeline-items="timelineItems"
-      :current-page="currentPage"
       ref="timelineRef"
     />
     <TheActivities v-show="currentPage === PAGE_ACTIVITIES" :activities="activities" />
     <TheProgress v-show="currentPage === PAGE_PROGRESS" />
   </main>
 
-  <TheNav :current-page="currentPage" @navigate="navigate" />
+  <TheNav />
 </template>
--- src/router.js
+++ src/router.js
@@ -9,9 +9,7 @@ export const currentPage = ref(normalizePageHash())
 export function navigate(page) {
   if (currentPage.value === PAGE_TIMELINE && page === PAGE_TIMELINE) {
     timelineRef.value.scrollToHour()
-  }
-
-  if (page !== PAGE_TIMELINE) {
+  } else if (page !== PAGE_TIMELINE) {
     document.body.scrollIntoView()
   }
 
