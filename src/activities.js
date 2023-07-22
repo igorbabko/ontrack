597fab50fdd07f974d04aa57c45bb1e64a8d7fa8
Igor Babko <i.i.babko@gmail.com>
2023-07-22 20:28:41 +0300

    81. Refactor activities module

--- src/activities.js
+++ src/activities.js
@@ -1,5 +1,6 @@
 import { ref, computed } from 'vue'
-import { generateActivities, generateActivitySelectOptions } from './functions'
+import { SECONDS_IN_HOUR } from './constants'
+import { id } from './functions'
 
 export const activities = ref(generateActivities())
 
@@ -15,4 +16,16 @@ export function deleteActivity(activity) {
 
 export function setActivitySecondsToComplete(activity, secondsToComplete) {
   activity.secondsToComplete = secondsToComplete || 0
+}
+
+function generateActivities() {
+  return ['Coding', 'Reading', 'Training'].map((name, hours) => ({
+    id: id(),
+    name,
+    secondsToComplete: hours * SECONDS_IN_HOUR
+  }))
+}
+
+function generateActivitySelectOptions(activities) {
+  return activities.map((activity) => ({ value: activity.id, label: activity.name }))
 }
--- src/functions.js
+++ src/functions.js
@@ -1,9 +1,4 @@
-import {
-  MILLISECONDS_IN_SECOND,
-  SECONDS_IN_HOUR,
-  SECONDS_IN_MINUTE,
-  MINUTES_IN_HOUR
-} from './constants'
+import { MILLISECONDS_IN_SECOND, SECONDS_IN_MINUTE, MINUTES_IN_HOUR } from './constants'
 import { isNull } from './validators'
 
 export function currentHour() {
@@ -24,14 +19,6 @@ export function normalizeSelectValue(value) {
   return isNull(value) || isNaN(value) ? value : +value
 }
 
-export function generateActivities() {
-  return ['Coding', 'Reading', 'Training'].map((name, hours) => ({
-    id: id(),
-    name,
-    secondsToComplete: hours * SECONDS_IN_HOUR
-  }))
-}
-
 export function id() {
   return Date.now().toString(36) + Math.random().toString(36).substring(2)
 }
@@ -40,10 +27,6 @@ export function getTotalActivitySeconds(activity, timelineItems) {
   return timelineItems
     .filter((timelineItem) => timelineItem.activityId === activity.id)
     .reduce((totalSeconds, timelineItem) => Math.round(timelineItem.activitySeconds + totalSeconds), 0)
-}
-
-export function generateActivitySelectOptions(activities) {
-  return activities.map((activity) => ({ value: activity.id, label: activity.name }))
 }
 
 export function generatePeriodSelectOptions() {
