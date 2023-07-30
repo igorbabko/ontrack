c14ce375e19dfeb7ed7c1c7e378159da038bf868
Igor Babko <i.i.babko@gmail.com>
2023-07-30 11:06:48 +0300

    89. Routing with dynamic component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 <script setup>
-import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from './constants'
-import { currentPage } from './router'
+import { routes, currentPage } from './router'
 import TheHeader from './components/TheHeader.vue'
 import TheNav from './components/TheNav.vue'
-import TheTimeline from './pages/TheTimeline.vue'
-import TheActivities from './pages/TheActivities.vue'
-import TheProgress from './pages/TheProgress.vue'
 </script>
 
 <template>
   <TheHeader />
 
   <main class="flex flex-grow flex-col">
-    <TheTimeline v-show="currentPage === PAGE_TIMELINE" />
-    <TheActivities v-show="currentPage === PAGE_ACTIVITIES" />
-    <TheProgress v-show="currentPage === PAGE_PROGRESS" />
+    <KeepAlive>
+      <component :is="routes[currentPage]" />
+    </KeepAlive>
   </main>
 
   <TheNav />
