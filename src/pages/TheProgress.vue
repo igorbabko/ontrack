bd0271bb9947ae1ff3fabc753c1959ecfc46f921
Igor Babko <i.i.babko@gmail.com>
2023-08-09 12:15:45 +0300

    94. Progress page template

--- src/pages/TheProgress.vue
+++ src/pages/TheProgress.vue
@@ -1,3 +1,44 @@
 <template>
-  <h1>Progress</h1>
+  <ul class="divide-y">
+    <li class="flex flex-col gap-1 p-4">
+      <div class="truncate text-xl">Activity 1</div>
+      <div class="flex h-5 overflow-hidden rounded bg-neutral-200">
+        <div class="bg-red-500" style="width: 10%" />
+      </div>
+      <div class="flex justify-between font-mono text-sm">
+        <span>10%</span>
+        <span>03:00 / 30:00</span>
+      </div>
+    </li>
+    <li class="flex flex-col gap-1 p-4">
+      <div class="truncate text-xl">Activity 2</div>
+      <div class="flex h-5 overflow-hidden rounded bg-neutral-200">
+        <div class="bg-yellow-500" style="width: 50%" />
+      </div>
+      <div class="flex justify-between font-mono text-sm">
+        <span>50%</span>
+        <span>15:00 / 30:00</span>
+      </div>
+    </li>
+    <li class="flex flex-col gap-1 p-4">
+      <div class="truncate text-xl">Activity 3</div>
+      <div class="flex h-5 overflow-hidden rounded bg-neutral-200">
+        <div class="bg-blue-500" style="width: 70%" />
+      </div>
+      <div class="flex justify-between font-mono text-sm">
+        <span>70%</span>
+        <span>21:00 / 30:00</span>
+      </div>
+    </li>
+    <li class="flex flex-col gap-1 p-4">
+      <div class="truncate text-xl">Activity 4</div>
+      <div class="flex h-5 overflow-hidden rounded bg-neutral-200">
+        <div class="bg-green-500" style="width: 100%" />
+      </div>
+      <div class="flex justify-between font-mono text-sm">
+        <span>100%</span>
+        <span>30:00 / 30:00</span>
+      </div>
+    </li>
+  </ul>
 </template>
