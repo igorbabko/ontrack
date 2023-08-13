86751b7ec8c32662b98dda3797f977fe23ac8a4a
Igor Babko <i.i.babko@gmail.com>
2023-08-13 15:03:21 +0300

    98. Decouple timeline items and activities modules

--- src/components/ProgressItem.vue
+++ src/components/ProgressItem.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { computed } from 'vue'
+import { HUNDRED_PERCENT } from '../constants'
 import { getProgressColorClass } from '../functions'
-import { getActivityProgress } from '../activities'
+import { calculateTrackedActivitySeconds } from '../timeline-items'
+import { calculateActivityCompletionPercentage } from '../activities'
 
 const props = defineProps(['index', 'activity'])
 
@@ -9,17 +11,24 @@ const timeProgress = ['03:00 / 30:00', '15:00 / 30:00', '21:00 / 30:00', '30:00 
   props.index
 ]
 
-const progress = computed(() => getActivityProgress(props.activity))
+const percentage = computed(() => {
+  const trackedActivitySeconds = calculateTrackedActivitySeconds(props.activity)
+
+  return calculateActivityCompletionPercentage(props.activity, trackedActivitySeconds)
+})
 </script>
 
 <template>
   <li class="flex flex-col gap-1 p-4">
     <div class="truncate text-xl">{{ activity.name }}</div>
     <div class="flex h-5 overflow-hidden rounded bg-neutral-200">
-      <div :class="getProgressColorClass(progress)" :style="`width: ${progress}%`" />
+      <div
+        :class="getProgressColorClass(percentage)"
+        :style="`width: ${Math.min(percentage, HUNDRED_PERCENT)}%`"
+      />
     </div>
     <div class="flex justify-between font-mono text-sm">
-      <span>{{ progress }}%</span>
+      <span>{{ percentage }}%</span>
       <span>{{ timeProgress }}</span>
     </div>
   </li>
