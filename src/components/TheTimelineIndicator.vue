3c9a6ff111021cb539ac1d23e421c78226f60425
Igor Babko <i.i.babko@gmail.com>
2023-08-24 11:08:15 +0300

    x - passes midnight

--- src/components/TheTimelineIndicator.vue
+++ src/components/TheTimelineIndicator.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed, onActivated, onDeactivated } from 'vue'
+import { ref, computed, watchEffect, onActivated, onDeactivated } from 'vue'
 import {
   MILLISECONDS_IN_SECOND,
   SECONDS_IN_MINUTE,
@@ -7,54 +7,58 @@ import {
   MINUTES_IN_HOUR,
   HUNDRED_PERCENT
 } from '../constants'
+import { currentDate } from '../functions'
 
 const secondsSinceMidnight = ref(getSecondsSinceMidnight())
-
 const indicatorRef = ref()
 
-let a = null
+let timer = null
 
 onActivated(() => {
-  console.log('activated')
+  secondsSinceMidnight.value = getSecondsSinceMidnight()
 
-  a = setInterval(
-    () => (secondsSinceMidnight.value = getSecondsSinceMidnight()),
-    MILLISECONDS_IN_SECOND
-  )
+  timer = setInterval(() => secondsSinceMidnight.value++, MILLISECONDS_IN_SECOND)
 })
 
-onDeactivated(() => {
-  console.log('deactivated')
+onDeactivated(() => clearInterval(timer))
 
-  clearInterval(a)
-})
+const topOffset = computed(
+  () => (secondsSinceMidnightInPercentage.value * timelineHeight()) / HUNDRED_PERCENT
+)
 
-const topOffset = computed(() => {
-  const secondsPercentage = (HUNDRED_PERCENT * secondsSinceMidnight.value) / SECONDS_IN_DAY
+const secondsSinceMidnightInPercentage = computed(
+  () => (HUNDRED_PERCENT * secondsSinceMidnight.value) / SECONDS_IN_DAY
+)
 
-  console.log(secondsSinceMidnight.value)
+function timelineHeight() {
+  if (indicatorRef.value) {
+    console.log(indicatorRef.value.parentNode.getBoundingClientRect().height)
+  }
 
-  return (
-    (secondsPercentage * indicatorRef.value?.parentNode?.getBoundingClientRect?.()?.height) /
-    HUNDRED_PERCENT
-  )
-})
+  return indicatorRef.value ? indicatorRef.value.parentNode.getBoundingClientRect().height : 0
+}
 
 function getSecondsSinceMidnight() {
-  const now = new Date()
+  const now = currentDate()
 
   return (
-    now.getSeconds() +
+    SECONDS_IN_MINUTE * MINUTES_IN_HOUR * now.getHours() +
     SECONDS_IN_MINUTE * now.getMinutes() +
-    SECONDS_IN_MINUTE * MINUTES_IN_HOUR * now.getHours()
+    now.getSeconds()
   )
 }
+
+watchEffect(() => {
+  if (secondsSinceMidnightInPercentage.value === HUNDRED_PERCENT) {
+    secondsSinceMidnight.value = getSecondsSinceMidnight()
+  }
+})
 </script>
 
 <template>
   <hr
     ref="indicatorRef"
-    class="pointer-events-none absolute z-10 w-full border-b-2 border-red-600 opacity-50"
+    class="pointer-events-none absolute z-20 w-full border-b-2 border-red-600 opacity-50"
     :style="{ top: `${topOffset}px` }"
   />
 </template>
