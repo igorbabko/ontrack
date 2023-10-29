4340ea24e41201541f3da825745648e97ffefc41
Igor Babko <i.i.babko@gmail.com>
2023-10-29 12:58:16 +0200

    110

--- src/components/TheTimelineIndicator.vue
+++ src/components/TheTimelineIndicator.vue
@@ -1,25 +1,22 @@
 <script setup>
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 import { SECONDS_IN_MINUTE, SECONDS_IN_DAY, MINUTES_IN_HOUR, HUNDRED_PERCENT } from '../constants'
 import { currentDate } from '../functions'
 
-const secondsSinceMidnight = ref(getSecondsSinceMidnight())
 const indicatorRef = ref()
 
-const topOffset = computed(
-  () => (secondsSinceMidnightInPercentage.value * timelineHeight()) / HUNDRED_PERCENT
-)
-
-const secondsSinceMidnightInPercentage = computed(
-  () => (HUNDRED_PERCENT * secondsSinceMidnight.value) / SECONDS_IN_DAY
-)
-
-function timelineHeight() {
-  return indicatorRef.value?.parentNode.getBoundingClientRect().height ?? 0
+function calculateTopOffset() {
+  return (calculateSecondsSinceMidnightInPercentage() * getTimelineHeight()) / HUNDRED_PERCENT
 }
 
-function getSecondsSinceMidnight() {
+function calculateSecondsSinceMidnightInPercentage() {
+  return (HUNDRED_PERCENT * calculateSecondsSinceMidnight()) / SECONDS_IN_DAY
+}
+
+function calculateSecondsSinceMidnight() {
   const now = currentDate()
+
+  now.setHours(9);
 
   return (
     SECONDS_IN_MINUTE * MINUTES_IN_HOUR * now.getHours() +
@@ -27,12 +24,16 @@ function getSecondsSinceMidnight() {
     now.getSeconds()
   )
 }
+
+function getTimelineHeight() {
+  return indicatorRef.value?.parentNode.getBoundingClientRect().height
+}
 </script>
 
 <template>
   <hr
     ref="indicatorRef"
     class="pointer-events-none absolute z-10 w-full border-b-2 border-red-600"
-    :style="{ top: `${topOffset}px` }"
+    :style="{ top: `${calculateTopOffset()}px` }"
   />
 </template>
