44c02d9248e08f09a026fcc6a00f8d371ce4fd8b
Igor Babko <i.i.babko@gmail.com>
2023-11-01 11:06:41 +0200

    113. Seconds since midnight composable

--- src/components/TheTimelineIndicator.vue
+++ src/components/TheTimelineIndicator.vue
@@ -1,51 +1,15 @@
 <script setup>
-import { ref, computed, watchEffect, onActivated, onDeactivated } from 'vue'
-import {
-  HUNDRED_PERCENT,
-  SECONDS_IN_DAY,
-  SECONDS_IN_MINUTE,
-  MINUTES_IN_HOUR,
-  MILLISECONDS_IN_SECOND
-} from '../constants'
+import { ref, computed } from 'vue'
+import { HUNDRED_PERCENT } from '../constants'
+import { useSecondsSinceMidnight } from '../composables/seconds-since-midnight'
 
-const secondsSinceMidnight = ref(calculateSecondsSinceMidnight())
 const indicatorRef = ref()
 
-let timer = null
-
-onActivated(() => {
-  secondsSinceMidnight.value = calculateSecondsSinceMidnight()
-
-  timer = setInterval(() => {
-    secondsSinceMidnight.value += 5 * 60
-  }, MILLISECONDS_IN_SECOND)
-})
-
-onDeactivated(() => clearInterval(timer))
+const { secondsSinceMidnightInPercentage } = useSecondsSinceMidnight()
 
 const topOffset = computed(
   () => (secondsSinceMidnightInPercentage.value * getTimelineHeight()) / HUNDRED_PERCENT
 )
-
-const secondsSinceMidnightInPercentage = computed(
-  () => (HUNDRED_PERCENT * secondsSinceMidnight.value) / SECONDS_IN_DAY
-)
-
-watchEffect(() => {
-  if (secondsSinceMidnight.value > SECONDS_IN_DAY) {
-    secondsSinceMidnight.value = 0
-  }
-})
-
-function calculateSecondsSinceMidnight() {
-  const now = new Date()
-
-  return (
-    SECONDS_IN_MINUTE * MINUTES_IN_HOUR * now.getHours() +
-    SECONDS_IN_MINUTE * now.getMinutes() +
-    now.getSeconds()
-  )
-}
 
 function getTimelineHeight() {
   return indicatorRef.value?.parentNode.getBoundingClientRect().height
