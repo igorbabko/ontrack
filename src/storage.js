4672bc24807e70b008135b52bddd63ec3bfc5953
Igor Babko <i.i.babko@gmail.com>
2023-11-15 13:47:12 +0200

    124. Sync idle seconds

--- src/storage.js
+++ src/storage.js
@@ -1,4 +1,4 @@
-import { APP_NAME } from './constants'
+import { APP_NAME, MILLISECONDS_IN_SECOND } from './constants'
 import { isToday, today } from './time'
 import { timelineItems } from './timeline-items'
 import { activities } from './activities'
@@ -8,8 +8,13 @@ export function loadState() {
 
   const state = serializedState ? JSON.parse(serializedState) : {}
 
-  timelineItems.value = isToday(new Date(state.date)) ? state.timelineItems : timelineItems.value
   activities.value = state.activities || activities.value
+
+  const lastActiveAt = new Date(state.lastActiveAt)
+
+  timelineItems.value = isToday(lastActiveAt)
+    ? syncIdleSeconds(state.timelineItems, lastActiveAt)
+    : timelineItems.value
 }
 
 export function saveState() {
@@ -18,7 +23,17 @@ export function saveState() {
     JSON.stringify({
       timelineItems: timelineItems.value,
       activities: activities.value,
-      date: today()
+      lastActiveAt: today()
     })
   )
+}
+
+function syncIdleSeconds(timelineItems, lastActiveAt) {
+  const activeTimelineItem = timelineItems.find(({ isActive }) => isActive)
+
+  if (activeTimelineItem) {
+    activeTimelineItem.activitySeconds += (today() - lastActiveAt) / MILLISECONDS_IN_SECOND
+  }
+
+  return timelineItems
 }
