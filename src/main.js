38a3a6bef8a0fc21bed000b77b2b3fff543fa294
Igor Babko <i.i.babko@gmail.com>
2023-11-30 09:41:43 +0200

    135

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import { syncState } from './storage'
+import { startCurrentDateTimer } from './time'
 import App from './App.vue'
 
 import './assets/main.css'
 
 syncState()
+
+startCurrentDateTimer()
 
 document.addEventListener('visibilitychange', () =>
   syncState(document.visibilityState === 'visible')
